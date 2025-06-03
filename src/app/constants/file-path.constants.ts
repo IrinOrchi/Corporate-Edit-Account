d895cf3b2d51291635d103812714f928004ec463
IrinOrchi <irin.hoque.orchi@g.bracu.ac.bd>
2025-06-03 10:31:41 +0600

    file path value updated

--- src/app/constants/file-path.constants.ts
+++ src/app/constants/file-path.constants.ts
@@ -496,16 +496,16 @@ export const filePath: { [key: string]: string } = {
 
  
 export const disabilities = [
-        { label: 'Accessible documentation and alternative formats', value: '1' },
-        { label: 'Accessible Washrooms / Toilets', value: '2' },
-        { label: 'Adapted Transport facility for Distant Travelling', value: '3' },
-        { label: 'Assistive Software, communication and computer devices', value: '4' },
-        { label: 'Available Flexible working shifts', value: '5' },
-        { label: 'Offering Work from home', value: '6' },
-        { label: 'Ramps or Lifts or Escalators for entry and move between floors', value: '7' },
-        { label: 'Reasonable Accommodation in Recruitment/interview procedures like sign language, oral/typed/video interview', value: '8' },
-        { label: 'Warning Indicators or Markers in place for hazards, staircase', value: '9' },
-        { label: 'Workstation or seating adaptations for easy use', value: '0' }
+        { label: 'Accessible documentation and alternative formats', value: '4' },
+        { label: 'Accessible Washrooms / Toilets', value: '5' },
+        { label: 'Adapted Transport facility for Distant Travelling', value: '6' },
+        { label: 'Assistive Software, communication and computer devices', value: '7' },
+        { label: 'Available Flexible working shifts', value: '8' },
+        { label: 'Offering Work from home', value: '9' },
+        { label: 'Ramps or Lifts or Escalators for entry and move between floors', value: '10' },
+        { label: 'Reasonable Accommodation in Recruitment/interview procedures like sign language, oral/typed/video interview', value: '11' },
+        { label: 'Warning Indicators or Markers in place for hazards, staircase', value: '12' },
+        { label: 'Workstation or seating adaptations for easy use', value: '13' }
       ];
       
     
